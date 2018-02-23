9532dfd960b6fab76a77843a82623e9901f14358
George <gabolaev98@gmail.com>
2018-02-23 16:36:05 +0300

    Added script.js, css, media and fonts

---
+++ public/static/js/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const back = document.getElementById('back');
+const application = document.getElementById('application');
+const menuSection = document.getElementById('menu');
+const signinSection = document.getElementById('signin');
+const signupSection = document.getElementById('signup');
+const multiplayerSection = document.getElementById('multiplayer');
+const singleplayerSection = document.getElementById('singleplayer');
+const scoreboardSection = document.getElementById('scoreboard');
+const rulesSection = document.getElementById('rules');
+
+
+const sections = {
+    menu: menuSection,
+    signin: signinSection,
+    signup: signupSection,
+    multiplayer: multiplayerSection,
+    singleplayer: singleplayerSection,
+    scoreboard: scoreboardSection,
+    rules: rulesSection,
+};
+
+function hideAllExcept(section) {
+    Object.entries(sections).forEach(
+        ([key, value]) => value.hidden = (section !== key)
+    );
+    back.hidden = (section === 'menu');
+}
+
+application.addEventListener('click', function (event) {
+    const target = event.target;
+    const sectionName = target.getAttribute('data-section');
+    if (target.tagName.toLowerCase() === 'a') {
+        openSection(sectionName);
+    }
+});
+
+function openSection(section) {
+    hideAllExcept(section);
+}
+
+openSection('menu');
