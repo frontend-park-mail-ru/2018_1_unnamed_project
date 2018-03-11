5e70c876b410abcc9bec61c1f20c325acaf0332a
George <gabolaev98@gmail.com>
2018-03-11 16:19:50 +0300

    Added settings page + avatar updating and removing

--- public/static/js/components/settings/Settings.js
+++ public/static/js/components/settings/Settings.js
@@ -1,17 +1,35 @@
 (function() {
-
+    /**
+     * User profile settings
+     */
     class SettingsBuilder extends window.AbstractBuilder {
+        /**
+         * @param {string} selector
+         */
         constructor(selector) {
             super(selector);
         }
 
+        // noinspection JSMethodCanBeStatic
+        /**
+         * Метод для обновления строки с аватаром пользователя.
+         */
+        updateBar() {
+            window.signinPage.builder.checkAuth();
+        }
+
+        /**
+         *
+         */
         render() {
-            const avatarLink = (this._data.avatarLink ? (backendURL + this._data.avatarLink) : 'https://www.shareicon.net/data/128x128/2016/08/05/806962_user_512x512.png');
-            this._node.innerHTML = `
+            const backendURI = this.api.backendURI;
+            console.log(backendURI);
+            const avatarLink = (this.data.avatarLink ? (backendURI + this.data.avatarLink) : 'https://www.shareicon.net/data/128x128/2016/08/05/806962_user_512x512.png');
+            this.node.innerHTML = `
             DEEEESIIIIIIIGGGGGGNNNNNNNNNNNN
             <div class="img-with-text">
                 <img class="avatar" src="${avatarLink}"/>
-                <form action="${backendURL + '/me/avatar/'}" method="POST" id="upload-avatar" enctype="multipart/form-data">
+                <form action="${backendURI + '/me/avatar/'}" method="POST" id="upload-avatar" enctype="multipart/form-data">
                     <span class="upload-btn-wrapper">
                         <button class="btn update">Update</button>
                         <input type="file" name="avatar"/>
@@ -19,24 +37,32 @@
                 </form>
                 <a class="btn delete" id="delete-avatar">Delete</a>
             </div>
-            <form action="${backendURL + '/me/'}" class="settings__form" method="PATCH" id="update-info">
-                <input type="text" name="username" placeholder="${this._data.username}"/>
-                <input type="text" name="email" placeholder="${this._data.email}"/>
+            <form action="${backendURI + '/me/'}" class="settings__form" method="PATCH" id="update-info">
+                <input type="text" name="username" placeholder="${this.data.username}"/>
+                <input type="text" name="email" placeholder="${this.data.email}"/>
                 <input type="password" name="password" placeholder="new password"/>
                 <input type="password_confirmation" name="password_confirmation" placeholder="again"/>
             </form>
             <a href="#" id="logout" data-section="menu">LOG OUT</a>
             `;
 
-            document.getElementById('logout').addEventListener('click', signinBuilder.logoutMe);
+
+            const signinBuilder = window.application.signinPage.builder;
+
+            document.getElementById('logout').addEventListener('click', signinBuilder.logoutMe.bind(signinBuilder));
             const form = document.getElementById('upload-avatar');
             form.addEventListener('change', () => this.setAvatar(form));
-            document.getElementById('delete-avatar').addEventListener('click', this.removeAvatar);
+            document.getElementById('delete-avatar').addEventListener('click', this.removeAvatar.bind(this));
+        };
 
-        }
-
+        /**
+         * Удаляет аватар пользователя.
+         */
         removeAvatar() {
-            api.deleteAvatar()
+            const settingsBuilder = window.application.settingsPage.builder;
+            const push = window.application.push;
+
+            this.api.deleteAvatar()
                 .then((response) => {
                     settingsBuilder.data = response;
                     settingsBuilder.render();
@@ -48,9 +74,17 @@
                 });
         }
 
+        /**
+         * Устанавливает аватар пользователя.
+         * @param {Object} form
+         */
         setAvatar(form) {
-            api.uploadAvatar(form)
+            const settingsBuilder = window.application.settingsPage.builder;
+            const push = window.application.push;
+
+            this.api.uploadAvatar(form)
                 .then((response) => {
+                    debugger;
                     settingsBuilder.data = response;
                     settingsBuilder.render();
                     push.data = 'Avatar updated';
@@ -59,7 +93,7 @@
                 .catch((error) => {
                     console.error(error);
                 });
-        }
+        };
     }
 
     window.SettingsBuilder = SettingsBuilder;
--- public/static/js/modules/validator.js
+++ public/static/js/modules/validator.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     /**
      * Validate fields for signin/signup/setting
      */
