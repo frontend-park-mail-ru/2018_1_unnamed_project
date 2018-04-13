6e23cd4b7e8e8e42d655e93c16bda83572c98269
jpyatachkov <cvkucherov@yandex.ru>
2018-04-13 10:37:54 +0300

    server fixes

--- server/server.js
+++ server/server.js
@@ -1,19 +1,17 @@
-/* eslint-disable */
 const path = require('path');
 const body = require('body-parser');
 const cookie = require('cookie-parser');
 const debug = require('debug');
 const express = require('express');
 const app = express();
-const uuid = require('uuid/v4');
 const logger = debug('mylogger');
 
-const public = __dirname + '/../public/';
+const publicDir =  `${__dirname}/../public/`;
 
 app.use(body.json());
 app.use(cookie());
 
-app.use('/sw.js', express.static(path.join(__dirname + '/../dist/sw.js')));
+app.use('/sw.js', express.static(path.join(__dirname, '/../dist/sw.js')));
 
 app.get('/dist/*', (req, res) => {
     logger(`BUNDLE: ${req.url} ${req.method}`);
@@ -21,13 +19,14 @@ app.get('/dist/*', (req, res) => {
 });
 
 app.get('/media/*', (req, res) => {
-    logger(`STATIC FILE: ${req.url} ${req.method} ${path.join(public, 'static', req.url)}`);
-    res.sendFile(path.join(public, 'static', req.url));
+    logger(`STATIC FILE: ${req.url} ${req.method} ${path.join(publicDir, 'static', req.url)}`);
+    res.sendFile(path.join(publicDir, 'static', req.url));
 });
 
 app.get('/*', (req, res) => {
     logger(`${req.url} ${req.method}`);
-    res.sendFile(path.join(public, 'index.html'));
+    res.sendFile(path.join(publicDir, 'index.html'));
 });
 
+// noinspection ES6ModulesDependencies
 app.listen(process.env.PORT || 5000);
